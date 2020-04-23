5716a936229fb6d535982ea1de6dc6573f88a7cc
Alexander Dimitrov <alexander_dimitrov@yahoo.com>
2020-04-23 12:24:27 +0300

    TempDeleterUploaded

---
+++ ToolBoxGNA/GNA/ToolBox GNA/Controller/TempCleaner/Service/TempFileDeleterService/TempFileDeleterService.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TempDeleter.ViewModel.TempFileDeleterService;
+
+namespace TempDeleter
+{
+    public class TempFileDeleterService : ITempFileDeleterService
+    {
+        TempFIle model = new TempFIle();
+        int count = 0;
+        StringBuilder sb = new StringBuilder();
+        public void TempFileRemover(string FolderName)
+        {
+            DirectoryInfo dir = new DirectoryInfo(FolderName);
+            string name = null;
+
+            foreach (FileInfo file in dir.GetFiles())
+            {
+                try
+                {
+                    name = file.Name;
+
+                    file.Delete();
+                }
+                catch (UnauthorizedAccessException)
+
+                {
+                    count++;
+                    sb.Append(name + ", ");
+                }
+                catch (System.IO.IOException)
+                {
+                    count++;
+                    sb.Append(name + ", ");
+                }
+            }
+
+            foreach (DirectoryInfo directory in dir.GetDirectories())
+            {
+                try
+                {
+
+                    TempFileRemover(directory.FullName);
+
+                    directory.Delete(true);
+                }
+                catch (UnauthorizedAccessException)
+                {
+
+                }
+                catch (System.IO.IOException)
+                {
+
+                }
+
+            }
+
+            model.Count = count;
+            model.Names = sb.ToString();
+
+                
+        }
+
+        public void Reset()
+        {
+            count = 0;
+            sb.Clear();
+        }
+
+        public void GetModelNames()
+        {
+            MessageBox.Show($"{model.Names}");
+        }
+
+        public bool HasErrors()
+        {
+            
+            if (model.Count > 0)
+            {
+                MessageBox.Show($"{model.Count} files COULDN'T BE DELETED BECAUSE THEY ARE USED BY ANOTHER PROCESS!", "Error", MessageBoxButtons.OK);
+                return true;
+            }
+            else
+                return false;
+        }
+
+    }
+}
---
+++ ToolBoxGNA/GNA/ToolBox GNA/Controller/TempCleaner/TempDeleter.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TempDeleter
+{
+    public static class TempDeleter
+    {
+        static TempFileDeleterService service = new TempFileDeleterService();
+        public static async void DeleteTempFilesAsync(string folder)
+        {
+            await Task.Run(()=>service.TempFileRemover(folder));
+        }
+        public static async void GetErrorFilesNamesAsync()
+        {
+            await Task.Run(()=>service.GetModelNames());
+        }
+        public static bool HasErrors()
+        {
+            
+            return service.HasErrors();
+        }
+        public static void ResetValues()
+        {
+            service.Reset();
+        }
+       
+        public static bool HasTempFiles()
+        {
+            FileInfo[] tempDir = GetTempFileInfo(Path.GetTempPath());
+            
+            if (tempDir.Length > 0)
+            {
+
+                
+                MessageBox.Show($"Temp files found and are ready to be deleted.", "Scan complete.");
+                return true;
+            }
+            else
+            {
+                MessageBox.Show($"No temp files found to be deleted", "Scan Complete");
+                return false;
+            }
+        }
+        private static FileInfo[] GetTempFileInfo(string path)
+        {
+
+            DirectoryInfo directoryInfo = new DirectoryInfo(path);
+            FileInfo[] files = directoryInfo.GetFiles();
+            return files;
+        }
+    }
+}
