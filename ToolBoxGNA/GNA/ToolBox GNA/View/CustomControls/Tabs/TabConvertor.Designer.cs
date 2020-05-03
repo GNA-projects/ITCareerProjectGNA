fca2bcf11c03ecffef4be4b0b23c2cb7cb4e3168
Gosho Trapov <goshotrapov@gmail.com>
2020-05-03 14:00:27 +0300

    push

--- ToolBoxGNA/GNA/ToolBox GNA/View/CustomControls/Tabs/TabConvertor.Designer.cs
+++ ToolBoxGNA/GNA/ToolBox GNA/View/CustomControls/Tabs/TabConvertor.Designer.cs
@@ -28,123 +28,113 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.textBox1 = new System.Windows.Forms.TextBox();
-			this.textBox2 = new System.Windows.Forms.TextBox();
-			this.button1 = new System.Windows.Forms.Button();
-			this.button2 = new System.Windows.Forms.Button();
-			this.button3 = new System.Windows.Forms.Button();
-			this.button4 = new System.Windows.Forms.Button();
-			this.radioButton1 = new System.Windows.Forms.RadioButton();
-			this.radioButton2 = new System.Windows.Forms.RadioButton();
-			this.radioButton3 = new System.Windows.Forms.RadioButton();
-			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+			this.TbInput = new System.Windows.Forms.TextBox();
+			this.TbOutput = new System.Windows.Forms.TextBox();
+			this.BtnBrowseIn = new System.Windows.Forms.Button();
+			this.BtnBrowseOut = new System.Windows.Forms.Button();
+			this.BtnConvert = new System.Windows.Forms.Button();
+			this.RbJpgToPng = new System.Windows.Forms.RadioButton();
+			this.RbBmpToPng = new System.Windows.Forms.RadioButton();
+			this.RbWordToPdf = new System.Windows.Forms.RadioButton();
 			this.SuspendLayout();
 			// 
-			// textBox1
+			// TbInput
 			// 
-			this.textBox1.Location = new System.Drawing.Point(296, 53);
-			this.textBox1.Name = "textBox1";
-			this.textBox1.Size = new System.Drawing.Size(100, 20);
-			this.textBox1.TabIndex = 0;
+			this.TbInput.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(40)))), ((int)(((byte)(100)))));
+			this.TbInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.TbInput.Location = new System.Drawing.Point(64, 68);
+			this.TbInput.Name = "TbInput";
+			this.TbInput.Size = new System.Drawing.Size(339, 31);
+			this.TbInput.TabIndex = 0;
 			// 
-			// textBox2
+			// TbOutput
 			// 
-			this.textBox2.Location = new System.Drawing.Point(296, 102);
-			this.textBox2.Name = "textBox2";
-			this.textBox2.Size = new System.Drawing.Size(100, 20);
-			this.textBox2.TabIndex = 1;
+			this.TbOutput.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(40)))), ((int)(((byte)(100)))));
+			this.TbOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.TbOutput.Location = new System.Drawing.Point(64, 117);
+			this.TbOutput.Name = "TbOutput";
+			this.TbOutput.Size = new System.Drawing.Size(339, 31);
+			this.TbOutput.TabIndex = 1;
 			// 
-			// button1
+			// BtnBrowseIn
 			// 
-			this.button1.Location = new System.Drawing.Point(452, 51);
-			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(75, 23);
-			this.button1.TabIndex = 2;
-			this.button1.Text = "button1";
-			this.button1.UseVisualStyleBackColor = true;
+			this.BtnBrowseIn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.BtnBrowseIn.Location = new System.Drawing.Point(419, 68);
+			this.BtnBrowseIn.Name = "BtnBrowseIn";
+			this.BtnBrowseIn.Size = new System.Drawing.Size(80, 30);
+			this.BtnBrowseIn.TabIndex = 2;
+			this.BtnBrowseIn.Text = "Browse";
+			this.BtnBrowseIn.UseVisualStyleBackColor = true;
 			// 
-			// button2
+			// BtnBrowseOut
 			// 
-			this.button2.Location = new System.Drawing.Point(452, 100);
-			this.button2.Name = "button2";
-			this.button2.Size = new System.Drawing.Size(75, 23);
-			this.button2.TabIndex = 3;
-			this.button2.Text = "button2";
-			this.button2.UseVisualStyleBackColor = true;
+			this.BtnBrowseOut.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.BtnBrowseOut.Location = new System.Drawing.Point(419, 118);
+			this.BtnBrowseOut.Name = "BtnBrowseOut";
+			this.BtnBrowseOut.Size = new System.Drawing.Size(80, 30);
+			this.BtnBrowseOut.TabIndex = 3;
+			this.BtnBrowseOut.Text = "Browse";
+			this.BtnBrowseOut.UseVisualStyleBackColor = true;
 			// 
-			// button3
+			// BtnConvert
 			// 
-			this.button3.Location = new System.Drawing.Point(394, 359);
-			this.button3.Name = "button3";
-			this.button3.Size = new System.Drawing.Size(75, 23);
-			this.button3.TabIndex = 4;
-			this.button3.Text = "button3";
-			this.button3.UseVisualStyleBackColor = true;
+			this.BtnConvert.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.BtnConvert.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+			this.BtnConvert.Location = new System.Drawing.Point(308, 261);
+			this.BtnConvert.Name = "BtnConvert";
+			this.BtnConvert.Size = new System.Drawing.Size(191, 64);
+			this.BtnConvert.TabIndex = 4;
+			this.BtnConvert.Text = "Convert";
+			this.BtnConvert.UseVisualStyleBackColor = true;
 			// 
-			// button4
+			// RbJpgToPng
 			// 
-			this.button4.Location = new System.Drawing.Point(31, 359);
-			this.button4.Name = "button4";
-			this.button4.Size = new System.Drawing.Size(75, 23);
-			this.button4.TabIndex = 5;
-			this.button4.Text = "button4";
-			this.button4.UseVisualStyleBackColor = true;
+			this.RbJpgToPng.AutoSize = true;
+			this.RbJpgToPng.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.RbJpgToPng.Location = new System.Drawing.Point(64, 279);
+			this.RbJpgToPng.Name = "RbJpgToPng";
+			this.RbJpgToPng.Size = new System.Drawing.Size(137, 29);
+			this.RbJpgToPng.TabIndex = 6;
+			this.RbJpgToPng.TabStop = true;
+			this.RbJpgToPng.Text = ".jpg to .png";
+			this.RbJpgToPng.UseVisualStyleBackColor = true;
 			// 
-			// radioButton1
+			// RbBmpToPng
 			// 
-			this.radioButton1.AutoSize = true;
-			this.radioButton1.Location = new System.Drawing.Point(296, 188);
-			this.radioButton1.Name = "radioButton1";
-			this.radioButton1.Size = new System.Drawing.Size(85, 17);
-			this.radioButton1.TabIndex = 6;
-			this.radioButton1.TabStop = true;
-			this.radioButton1.Text = "radioButton1";
-			this.radioButton1.UseVisualStyleBackColor = true;
+			this.RbBmpToPng.AutoSize = true;
+			this.RbBmpToPng.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.RbBmpToPng.Location = new System.Drawing.Point(64, 314);
+			this.RbBmpToPng.Name = "RbBmpToPng";
+			this.RbBmpToPng.Size = new System.Drawing.Size(149, 29);
+			this.RbBmpToPng.TabIndex = 7;
+			this.RbBmpToPng.TabStop = true;
+			this.RbBmpToPng.Text = ".bmp to .png";
+			this.RbBmpToPng.UseVisualStyleBackColor = true;
 			// 
-			// radioButton2
+			// RbWordToPdf
 			// 
-			this.radioButton2.AutoSize = true;
-			this.radioButton2.Location = new System.Drawing.Point(296, 211);
-			this.radioButton2.Name = "radioButton2";
-			this.radioButton2.Size = new System.Drawing.Size(85, 17);
-			this.radioButton2.TabIndex = 7;
-			this.radioButton2.TabStop = true;
-			this.radioButton2.Text = "radioButton2";
-			this.radioButton2.UseVisualStyleBackColor = true;
-			// 
-			// radioButton3
-			// 
-			this.radioButton3.AutoSize = true;
-			this.radioButton3.Location = new System.Drawing.Point(296, 234);
-			this.radioButton3.Name = "radioButton3";
-			this.radioButton3.Size = new System.Drawing.Size(85, 17);
-			this.radioButton3.TabIndex = 8;
-			this.radioButton3.TabStop = true;
-			this.radioButton3.Text = "radioButton3";
-			this.radioButton3.UseVisualStyleBackColor = true;
-			// 
-			// richTextBox1
-			// 
-			this.richTextBox1.Location = new System.Drawing.Point(31, 51);
-			this.richTextBox1.Name = "richTextBox1";
-			this.richTextBox1.Size = new System.Drawing.Size(195, 249);
-			this.richTextBox1.TabIndex = 9;
-			this.richTextBox1.Text = "";
+			this.RbWordToPdf.AutoSize = true;
+			this.RbWordToPdf.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.RbWordToPdf.Location = new System.Drawing.Point(64, 244);
+			this.RbWordToPdf.Name = "RbWordToPdf";
+			this.RbWordToPdf.Size = new System.Drawing.Size(147, 29);
+			this.RbWordToPdf.TabIndex = 8;
+			this.RbWordToPdf.TabStop = true;
+			this.RbWordToPdf.Text = "Word to .pdf";
+			this.RbWordToPdf.UseVisualStyleBackColor = true;
 			// 
 			// TabConvertor
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Controls.Add(this.richTextBox1);
-			this.Controls.Add(this.radioButton3);
-			this.Controls.Add(this.radioButton2);
-			this.Controls.Add(this.radioButton1);
-			this.Controls.Add(this.button4);
-			this.Controls.Add(this.button3);
-			this.Controls.Add(this.button2);
-			this.Controls.Add(this.button1);
-			this.Controls.Add(this.textBox2);
-			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.RbWordToPdf);
+			this.Controls.Add(this.RbBmpToPng);
+			this.Controls.Add(this.RbJpgToPng);
+			this.Controls.Add(this.BtnConvert);
+			this.Controls.Add(this.BtnBrowseOut);
+			this.Controls.Add(this.BtnBrowseIn);
+			this.Controls.Add(this.TbOutput);
+			this.Controls.Add(this.TbInput);
 			this.Name = "TabConvertor";
 			this.ResumeLayout(false);
 			this.PerformLayout();
@@ -153,15 +143,13 @@
 
 		#endregion
 
-		private System.Windows.Forms.TextBox textBox1;
-		private System.Windows.Forms.TextBox textBox2;
-		private System.Windows.Forms.Button button1;
-		private System.Windows.Forms.Button button2;
-		private System.Windows.Forms.Button button3;
-		private System.Windows.Forms.Button button4;
-		private System.Windows.Forms.RadioButton radioButton1;
-		private System.Windows.Forms.RadioButton radioButton2;
-		private System.Windows.Forms.RadioButton radioButton3;
-		private System.Windows.Forms.RichTextBox richTextBox1;
+		private System.Windows.Forms.TextBox TbInput;
+		private System.Windows.Forms.TextBox TbOutput;
+		private System.Windows.Forms.Button BtnBrowseIn;
+		private System.Windows.Forms.Button BtnBrowseOut;
+		private System.Windows.Forms.Button BtnConvert;
+		private System.Windows.Forms.RadioButton RbJpgToPng;
+		private System.Windows.Forms.RadioButton RbBmpToPng;
+		private System.Windows.Forms.RadioButton RbWordToPdf;
 	}
 }
--- ToolBoxGNA/GNA/ToolBox GNA/View/CustomControls/Tabs/TabTempDeleter.Designer.cs
+++ ToolBoxGNA/GNA/ToolBox GNA/View/CustomControls/Tabs/TabTempDeleter.Designer.cs
@@ -28,10 +28,69 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TabTempDeleter));
+			this.BtnCheckDel = new System.Windows.Forms.Button();
+			this.RtbInfo = new System.Windows.Forms.RichTextBox();
+			this.LblExplain1 = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// BtnCheckDel
+			// 
+			this.BtnCheckDel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.BtnCheckDel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+			this.BtnCheckDel.Location = new System.Drawing.Point(45, 329);
+			this.BtnCheckDel.Name = "BtnCheckDel";
+			this.BtnCheckDel.Size = new System.Drawing.Size(228, 69);
+			this.BtnCheckDel.TabIndex = 0;
+			this.BtnCheckDel.Text = "Check";
+			this.BtnCheckDel.UseVisualStyleBackColor = true;
+			// 
+			// RtbInfo
+			// 
+			this.RtbInfo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(40)))), ((int)(((byte)(100)))));
+			this.RtbInfo.Location = new System.Drawing.Point(326, 15);
+			this.RtbInfo.Name = "RtbInfo";
+			this.RtbInfo.Size = new System.Drawing.Size(218, 405);
+			this.RtbInfo.TabIndex = 2;
+			this.RtbInfo.Text = "";
+			// 
+			// LblExplain1
+			// 
+			this.LblExplain1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+			this.LblExplain1.Location = new System.Drawing.Point(45, 15);
+			this.LblExplain1.Name = "LblExplain1";
+			this.LblExplain1.Size = new System.Drawing.Size(228, 169);
+			this.LblExplain1.TabIndex = 3;
+			this.LblExplain1.Text = resources.GetString("LblExplain1.Text");
+			// 
+			// label1
+			// 
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+			this.label1.Location = new System.Drawing.Point(45, 184);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(228, 132);
+			this.label1.TabIndex = 4;
+			this.label1.Text = resources.GetString("label1.Text");
+			// 
+			// TabTempDeleter
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.LblExplain1);
+			this.Controls.Add(this.RtbInfo);
+			this.Controls.Add(this.BtnCheckDel);
+			this.Name = "TabTempDeleter";
+			this.ResumeLayout(false);
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.Button BtnCheckDel;
+		private System.Windows.Forms.RichTextBox RtbInfo;
+		private System.Windows.Forms.Label LblExplain1;
+		private System.Windows.Forms.Label label1;
 	}
 }
