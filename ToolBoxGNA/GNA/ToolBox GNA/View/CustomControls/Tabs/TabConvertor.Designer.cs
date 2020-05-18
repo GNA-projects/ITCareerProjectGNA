0f5a730b5a406e7a5b6f33fde32ed8617d1c6bc1
Alexander Dimitrov <alexander_dimitrov@yahoo.com>
2020-05-18 20:02:53 +0300

    Fixed Colours

--- ToolBoxGNA/GNA/ToolBox GNA/View/CustomControls/Tabs/TabConvertor.Designer.cs
+++ ToolBoxGNA/GNA/ToolBox GNA/View/CustomControls/Tabs/TabConvertor.Designer.cs
@@ -43,27 +43,31 @@
             // 
             this.TbInput.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(40)))), ((int)(((byte)(100)))));
             this.TbInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.TbInput.Location = new System.Drawing.Point(34, 52);
+            this.TbInput.Location = new System.Drawing.Point(45, 64);
+            this.TbInput.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.TbInput.Name = "TbInput";
-            this.TbInput.Size = new System.Drawing.Size(380, 31);
+            this.TbInput.Size = new System.Drawing.Size(505, 37);
             this.TbInput.TabIndex = 0;
             // 
             // TbOutput
             // 
             this.TbOutput.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(40)))), ((int)(((byte)(100)))));
             this.TbOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.TbOutput.Location = new System.Drawing.Point(34, 119);
+            this.TbOutput.Location = new System.Drawing.Point(45, 146);
+            this.TbOutput.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.TbOutput.Name = "TbOutput";
-            this.TbOutput.Size = new System.Drawing.Size(380, 31);
+            this.TbOutput.Size = new System.Drawing.Size(505, 37);
             this.TbOutput.TabIndex = 1;
             // 
             // BtnBrowseIn
             // 
             this.BtnBrowseIn.FlatAppearance.BorderColor = System.Drawing.Color.White;
             this.BtnBrowseIn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.BtnBrowseIn.Location = new System.Drawing.Point(430, 52);
+            this.BtnBrowseIn.ForeColor = System.Drawing.SystemColors.Control;
+            this.BtnBrowseIn.Location = new System.Drawing.Point(573, 64);
+            this.BtnBrowseIn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.BtnBrowseIn.Name = "BtnBrowseIn";
-            this.BtnBrowseIn.Size = new System.Drawing.Size(80, 30);
+            this.BtnBrowseIn.Size = new System.Drawing.Size(107, 37);
             this.BtnBrowseIn.TabIndex = 2;
             this.BtnBrowseIn.Text = "Browse";
             this.BtnBrowseIn.UseVisualStyleBackColor = true;
@@ -73,9 +77,11 @@
             // 
             this.BtnBrowseOut.FlatAppearance.BorderColor = System.Drawing.Color.White;
             this.BtnBrowseOut.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.BtnBrowseOut.Location = new System.Drawing.Point(430, 120);
+            this.BtnBrowseOut.ForeColor = System.Drawing.SystemColors.Control;
+            this.BtnBrowseOut.Location = new System.Drawing.Point(573, 148);
+            this.BtnBrowseOut.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.BtnBrowseOut.Name = "BtnBrowseOut";
-            this.BtnBrowseOut.Size = new System.Drawing.Size(80, 30);
+            this.BtnBrowseOut.Size = new System.Drawing.Size(107, 37);
             this.BtnBrowseOut.TabIndex = 3;
             this.BtnBrowseOut.Text = "Browse";
             this.BtnBrowseOut.UseVisualStyleBackColor = true;
@@ -86,9 +92,11 @@
             this.BtnConvert.FlatAppearance.BorderColor = System.Drawing.Color.White;
             this.BtnConvert.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.BtnConvert.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
-            this.BtnConvert.Location = new System.Drawing.Point(319, 324);
+            this.BtnConvert.ForeColor = System.Drawing.SystemColors.Control;
+            this.BtnConvert.Location = new System.Drawing.Point(425, 399);
+            this.BtnConvert.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.BtnConvert.Name = "BtnConvert";
-            this.BtnConvert.Size = new System.Drawing.Size(191, 64);
+            this.BtnConvert.Size = new System.Drawing.Size(255, 79);
             this.BtnConvert.TabIndex = 4;
             this.BtnConvert.Text = "Convert";
             this.BtnConvert.UseVisualStyleBackColor = true;
@@ -98,21 +106,25 @@
             // 
             this.RbToPng.AutoSize = true;
             this.RbToPng.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.RbToPng.Location = new System.Drawing.Point(34, 359);
+            this.RbToPng.ForeColor = System.Drawing.SystemColors.Control;
+            this.RbToPng.Location = new System.Drawing.Point(45, 442);
+            this.RbToPng.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.RbToPng.Name = "RbToPng";
-            this.RbToPng.Size = new System.Drawing.Size(190, 29);
+            this.RbToPng.Size = new System.Drawing.Size(237, 35);
             this.RbToPng.TabIndex = 6;
-            this.RbToPng.TabStop = true;
             this.RbToPng.Text = ".jpg/.bmp to .png";
             this.RbToPng.UseVisualStyleBackColor = true;
             // 
             // RbWordToPdf
             // 
             this.RbWordToPdf.AutoSize = true;
+            this.RbWordToPdf.Checked = true;
             this.RbWordToPdf.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.RbWordToPdf.Location = new System.Drawing.Point(34, 324);
+            this.RbWordToPdf.ForeColor = System.Drawing.SystemColors.Control;
+            this.RbWordToPdf.Location = new System.Drawing.Point(45, 399);
+            this.RbWordToPdf.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.RbWordToPdf.Name = "RbWordToPdf";
-            this.RbWordToPdf.Size = new System.Drawing.Size(147, 29);
+            this.RbWordToPdf.Size = new System.Drawing.Size(182, 35);
             this.RbWordToPdf.TabIndex = 8;
             this.RbWordToPdf.TabStop = true;
             this.RbWordToPdf.Text = "Word to .pdf";
@@ -121,24 +133,28 @@
             // LblSource
             // 
             this.LblSource.AutoSize = true;
-            this.LblSource.Location = new System.Drawing.Point(31, 36);
+            this.LblSource.ForeColor = System.Drawing.SystemColors.Control;
+            this.LblSource.Location = new System.Drawing.Point(41, 44);
+            this.LblSource.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.LblSource.Name = "LblSource";
-            this.LblSource.Size = new System.Drawing.Size(66, 13);
+            this.LblSource.Size = new System.Drawing.Size(86, 17);
             this.LblSource.TabIndex = 9;
             this.LblSource.Text = "Source Path";
             // 
             // LblTarget
             // 
             this.LblTarget.AutoSize = true;
-            this.LblTarget.Location = new System.Drawing.Point(31, 103);
+            this.LblTarget.ForeColor = System.Drawing.SystemColors.Control;
+            this.LblTarget.Location = new System.Drawing.Point(41, 127);
+            this.LblTarget.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.LblTarget.Name = "LblTarget";
-            this.LblTarget.Size = new System.Drawing.Size(63, 13);
+            this.LblTarget.Size = new System.Drawing.Size(83, 17);
             this.LblTarget.TabIndex = 10;
             this.LblTarget.Text = "Target Path";
             // 
             // TabConvertor
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.LblTarget);
             this.Controls.Add(this.LblSource);
@@ -149,6 +165,7 @@
             this.Controls.Add(this.BtnBrowseIn);
             this.Controls.Add(this.TbOutput);
             this.Controls.Add(this.TbInput);
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.Name = "TabConvertor";
             this.ResumeLayout(false);
             this.PerformLayout();
--- ToolBoxGNA/GNA/ToolBox GNA/View/CustomControls/Tabs/TabTempDeleter.Designer.cs
+++ ToolBoxGNA/GNA/ToolBox GNA/View/CustomControls/Tabs/TabTempDeleter.Designer.cs
@@ -41,9 +41,11 @@
             this.BtnCheckDel.FlatAppearance.BorderColor = System.Drawing.Color.White;
             this.BtnCheckDel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.BtnCheckDel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
-            this.BtnCheckDel.Location = new System.Drawing.Point(15, 338);
+            this.BtnCheckDel.ForeColor = System.Drawing.SystemColors.Control;
+            this.BtnCheckDel.Location = new System.Drawing.Point(20, 416);
+            this.BtnCheckDel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.BtnCheckDel.Name = "BtnCheckDel";
-            this.BtnCheckDel.Size = new System.Drawing.Size(177, 69);
+            this.BtnCheckDel.Size = new System.Drawing.Size(236, 85);
             this.BtnCheckDel.TabIndex = 0;
             this.BtnCheckDel.Text = "Check";
             this.BtnCheckDel.UseVisualStyleBackColor = true;
@@ -52,27 +54,32 @@
             // RtbInfo
             // 
             this.RtbInfo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(40)))), ((int)(((byte)(100)))));
-            this.RtbInfo.Location = new System.Drawing.Point(326, 15);
+            this.RtbInfo.Location = new System.Drawing.Point(435, 18);
+            this.RtbInfo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.RtbInfo.Name = "RtbInfo";
-            this.RtbInfo.Size = new System.Drawing.Size(218, 405);
+            this.RtbInfo.Size = new System.Drawing.Size(289, 498);
             this.RtbInfo.TabIndex = 2;
             this.RtbInfo.Text = "";
             // 
             // LblExplain1
             // 
             this.LblExplain1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.LblExplain1.Location = new System.Drawing.Point(45, 15);
+            this.LblExplain1.ForeColor = System.Drawing.SystemColors.Control;
+            this.LblExplain1.Location = new System.Drawing.Point(60, 18);
+            this.LblExplain1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.LblExplain1.Name = "LblExplain1";
-            this.LblExplain1.Size = new System.Drawing.Size(228, 169);
+            this.LblExplain1.Size = new System.Drawing.Size(304, 208);
             this.LblExplain1.TabIndex = 3;
             this.LblExplain1.Text = resources.GetString("LblExplain1.Text");
             // 
             // label1
             // 
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.label1.Location = new System.Drawing.Point(45, 184);
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(60, 226);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(228, 132);
+            this.label1.Size = new System.Drawing.Size(304, 162);
             this.label1.TabIndex = 4;
             this.label1.Text = resources.GetString("label1.Text");
             // 
@@ -81,9 +88,11 @@
             this.BtnInfo.FlatAppearance.BorderColor = System.Drawing.Color.White;
             this.BtnInfo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.BtnInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
-            this.BtnInfo.Location = new System.Drawing.Point(215, 338);
+            this.BtnInfo.ForeColor = System.Drawing.SystemColors.Control;
+            this.BtnInfo.Location = new System.Drawing.Point(287, 416);
+            this.BtnInfo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.BtnInfo.Name = "BtnInfo";
-            this.BtnInfo.Size = new System.Drawing.Size(92, 69);
+            this.BtnInfo.Size = new System.Drawing.Size(123, 85);
             this.BtnInfo.TabIndex = 5;
             this.BtnInfo.Text = "Info";
             this.BtnInfo.UseVisualStyleBackColor = true;
@@ -91,13 +100,14 @@
             // 
             // TabTempDeleter
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.BtnInfo);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.LblExplain1);
             this.Controls.Add(this.RtbInfo);
             this.Controls.Add(this.BtnCheckDel);
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.Name = "TabTempDeleter";
             this.ResumeLayout(false);
 
