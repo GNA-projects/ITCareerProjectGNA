6ec4a70f3be21eaeb39cb9846b8e59be62cb2973
Gosho Trapov <goshotrapov@gmail.com>
2020-05-24 22:11:38 +0300

    Added

---
+++ ToolBoxGNA/GNA/DatabaseOperations/Operations/PasswordManagerBuissiness/PasswordManagerServices.cs
@@ -0,0 +1,29 @@
+﻿using DatabaseOperations.Model;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseOperations.Operations.PasswordManagerBuissiness
+{
+	public static class PasswordManagerServices
+	{
+		public static void AddPasswordOperation()
+		{
+			using (GNAEntities context = new GNAEntities())
+			{
+				Users user = context.Users.FirstOrDefault(x => x.id == CurrentUser.ID);
+				Operation_Info operation = new Operation_Info()
+				{
+					operation_type_id = 5,
+					operation_id = 12,
+					isSuccessfull = true
+				};
+				user.Operation_Info.Add(operation);
+				context.SaveChanges();
+			}
+		}
+	}
+}
