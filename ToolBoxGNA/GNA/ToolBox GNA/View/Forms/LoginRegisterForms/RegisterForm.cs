0891f108ccb6433f8d5f89bd9a867c5aa4a8db85
NikolaHadzhiev <46840723+NikolaHadzhiev@users.noreply.github.com>
2020-05-24 14:19:16 +0300

    Added  register validation for username and password

--- ToolBoxGNA/GNA/ToolBox GNA/View/Forms/LoginRegisterForms/RegisterForm.cs
+++ ToolBoxGNA/GNA/ToolBox GNA/View/Forms/LoginRegisterForms/RegisterForm.cs
@@ -3,6 +3,8 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity.Validation;
 using System.Drawing;
 using System.Linq;
 using System.Management;
@@ -21,7 +23,20 @@ namespace ToolBox_GNA.View.Forms.LoginRegisterForms
         }
         private void ButtonRegister_Click(object sender, EventArgs e)
         {
-            RegisterService.RegisterUser(TextBoxUsername.Text, TextBoxPassword.Text);
+            try
+            {
+                RegisterService.RegisterUser(TextBoxUsername.Text, TextBoxPassword.Text);
+            }
+            catch (DbUpdateException)
+            {
+                MessageBox.Show("Username taken! Choose another username", "Taken username!", 
+                                MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (DbEntityValidationException)
+            {
+                MessageBox.Show("Username or password is too long. Please choose a smaller username or password", "Too long credentials!", 
+                                MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
             this.Hide();
             LoginForm loginForm = new LoginForm();
             loginForm.Show();
