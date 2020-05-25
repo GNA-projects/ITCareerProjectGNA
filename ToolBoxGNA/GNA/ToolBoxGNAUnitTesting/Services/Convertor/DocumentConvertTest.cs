b77ad0b0425f2240ca7cdba00f280cc2e278af2d
Alexander Dimitrov <alexander_dimitrov@yahoo.com>
2020-05-25 15:08:35 +0300

    Added some tests

---
+++ ToolBoxGNA/GNA/ToolBoxGNAUnitTesting/Services/Convertor/DocumentConvertTest.cs
@@ -0,0 +1,54 @@
+﻿using ConverterDemo.Services;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToolBoxGNAUnitTesting.Services.Convertor
+{
+    [TestFixture()]
+    public class DocumetConvertorServiceTests
+    {
+        [TestCase(@"C:\Users\Admin\Desktop\TestFolderReadOnly\Test.docx.docx", @"C:\Users\Admin\Desktop\TestFolderReadOnly")]
+
+        public void DocumentConvertor_CorrectCase_SameFolder(string input, string output)
+        {
+            DocumetConvertorService service = new DocumetConvertorService();
+            service.WordToPDFConv(input, output);
+            FileAssert.Exists(@"C:\Users\Admin\Desktop\TestFolderReadOnly\Test.pdf.pdf");
+        }
+        [TestCase(@"C:\Users\Admin\Desktop\TestFolderReadOnly\Test.docx.docx", @"C:\Users\Admin\Desktop\New folder")]
+        public void DocumentConvertor_CorrectCase_DifferentOutputFolder(string input, string output)
+        {
+            DocumetConvertorService service = new DocumetConvertorService();
+            service.WordToPDFConv(input, output);
+            FileAssert.Exists(@"C:\Users\Admin\Desktop\New folder\Test.pdf.pdf");
+        }
+        [TestCase(@"C:\Users\Admin\Desktop\TestFolderReadOnly\", @"C:\Users\Admin\Desktop\New folder")]
+        public void DocumentConvertor_IncorrectCae_NoFileFound(string input, string output)
+        {
+            DocumetConvertorService service = new DocumetConvertorService();
+            FieldInfo[] fields = typeof(DocumetConvertorService).GetFields(BindingFlags.Instance | BindingFlags.NonPublic);
+            FieldInfo field = fields.FirstOrDefault(x => x.Name == "isDone");
+
+            field.SetValue(service, false);
+            service.WordToPDFConv(input, output);
+            Assert.IsFalse((bool)field.GetValue(service));
+
+
+        }
+        [TestCase(@"C:\Users\Admin\Desktop\TestFolderReadOnly\Test.docx.docx", @"C:\Users\Admin\Desktop\New folder\dox.docx")]
+        public void DocumentConvertor_IncorrectCae_OtherException(string input, string output)
+        {
+            DocumetConvertorService service = new DocumetConvertorService();
+            FieldInfo[] fields = typeof(DocumetConvertorService).GetFields(BindingFlags.Instance | BindingFlags.NonPublic);
+            FieldInfo field = fields.FirstOrDefault(x => x.Name == "isDone");
+            field.SetValue(service, false);
+            service.WordToPDFConv(input, output);
+            Assert.IsFalse((bool)field.GetValue(service));
+        }
+    }
+}
---
+++ ToolBoxGNA/GNA/ToolBoxGNAUnitTesting/Services/Convertor/ImageConvertTests.cs
@@ -0,0 +1,70 @@
+﻿using ConverterDemo.Services;
+using DatabaseOperations;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToolBoxGNAUnitTesting.Services.Convertor
+{
+
+    [TestFixture()]
+    public class ImageToPNGServiceTests
+    {
+        [TestCase(@"C:\Users\Admin\Desktop\ImageConvTests\BmpImage.bmp", @"C:\Users\Admin\Desktop\ImageConvertOutput")]
+        public void ImageConvert_Correctcase_bmp(string input, string output)
+        {
+            ImageToPNGService service = new ImageToPNGService();
+            CurrentUser.ID = 1;
+            CurrentUser.Username = "Test";
+            service.ImageConvert(input, output);
+
+            FileAssert.Exists(@"C:\Users\Admin\Desktop\ImageConvertOutput\BmpImage.png");
+        }
+        [TestCase(@"C:\Users\Admin\Desktop\ImageConvTests\download.jpg", @"C:\Users\Admin\Desktop\ImageConvertOutput")]
+        public void ImageConvert_Correctcase_jpg(string input, string output)
+        {
+            CurrentUser.Username = "Test";
+            ImageToPNGService service = new ImageToPNGService();
+            service.ImageConvert(input, output);
+
+            FileAssert.Exists(@"C:\Users\Admin\Desktop\ImageConvertOutput\download.png");
+        }
+        [TestCase(@"C:\Users\Admin\Desktop\ImageConvTests\PngImage.png", @"C:\Users\Admin\Desktop\ImageConvertOutput")]
+        public void ImageConvert_Correctcase_png(string input, string output)
+        {
+            CurrentUser.Username = "Test";
+            ImageToPNGService service = new ImageToPNGService();
+            service.ImageConvert(input, output);
+
+            FileAssert.Exists(@"C:\Users\Admin\Desktop\ImageConvertOutput\PngImage.png");
+        }
+        [TestCase(@"C:\Users\Admin\Desktop\ImageConvTests\", @"C:\Users\Admin\Desktop\ImageConvertOutput")]
+        public void ImageConvert_Incorrectcase_fileNotExist(string input, string output)
+        {
+            CurrentUser.Username = "Test";
+            ImageToPNGService service = new ImageToPNGService();
+            FieldInfo[] fields = typeof(ImageToPNGService).GetFields(BindingFlags.Instance | BindingFlags.NonPublic);
+            var field = fields.FirstOrDefault(x => x.Name == "isDone");
+            field.SetValue(service, false);
+            service.ImageConvert(input, output);
+            Assert.IsFalse((bool)field.GetValue(service));
+
+        }
+        [TestCase(@"C:\Users\Admin\Desktop\ImageConvTests\PngImage.png", " ")]
+        public void ImageConvert_Incorrectcase_OutPutNotExist(string input, string output)
+        {
+            CurrentUser.Username = "Test";
+            ImageToPNGService service = new ImageToPNGService();
+            FieldInfo[] fields = typeof(ImageToPNGService).GetFields(BindingFlags.Instance | BindingFlags.NonPublic);
+            var field = fields.FirstOrDefault(x => x.Name == "isDone");
+            field.SetValue(service, false);
+            service.ImageConvert(input, output);
+            Assert.IsFalse((bool)field.GetValue(service));
+
+        }
+    }
+}
